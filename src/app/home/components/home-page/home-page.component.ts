48e0cef8f3dd0dee057ce9a4b9a2d50a88292730
Nacho1886 <francert@hotmail.com>
2022-11-22 17:00:11 +0100

    Mistake in formGroup

--- src/app/home/components/home-page/home-page.component.ts
+++ src/app/home/components/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor( private formBuilder: FormBuilder ) { }
+  
+  
+  myForm: FormGroup = this.formBuilder.group({
+    webPage: [false, Validators.required],
+    seoCampaign: [false, Validators.required],
+    adsCampaign: [false, Validators.required],
+    pages: [1, Validators.required],
+    languages: [1, Validators.required]
+  })
+  
+  
+  
+
 
   ngOnInit(): void {
   }
