29627ac689a52fc6828888413e808b42f3df5f53
Nacho1886 <francert@hotmail.com>
2022-11-22 12:52:18 +0100

    Styles finished for moment

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PanelComponent } from './components/panel/panel.component';
 import { SharedModule } from '../shared/shared.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,8 @@ import { HomePageComponent } from './components/home-page/home-page.component';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
   exports: [
     HomePageComponent
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { RouterModule } from '@angular/router';
 
 
 
 @NgModule({
   declarations: [WelcomePageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [WelcomePageComponent]
 })
